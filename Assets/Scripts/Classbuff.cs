fa13c3fe656d3d0df5296e983882bcf067736cbe
duynp <duynguyen04302004@gmail.com>
2025-11-04 22:23:54 +0700

    update

---
+++ Assets/Scripts/Classbuff.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    public class Classbuff
+    {
+       public class ClassA
+        {
+            public void MethodA()
+            {
+                // Implementation of MethodA
+            }
+        }
+        public class ClassB
+        {
+            public void MethodB()
+            {
+                // Implementation of MethodB
+            }
+        }
+        public class ClassC
+        {
+            public void MethodC()
+            {
+                // Implementation of MethodC
+            }
+        }
+        public class ClassD
+        {
+            public void MethodD()
+            {
+                // Implementation of MethodD
+            }
+        }
+    }
+}
